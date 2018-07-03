f88bebbc82371a1314aa08a5ac77be793e712f5f
Ispire <ispirett@gmail.com>
2018-07-03 07:13:13 -0700

    firebase test
    testing firebase database. you must install python - firebase and install.

---
+++ firebasetest.py
@@ -0,0 +1,21 @@
+from firebase import firebase
+
+firebase = firebase.FirebaseApplication('link to your firebase database')
+firebase = firebase.FirebaseApplication('link two database goes here ')
+
+
+# writing to firebase
+def writeToFirebase(key, value):
+    user_input = {key: value}
+    send_input = firebase.post('user', user_input)
+    print(send_input)
+
+
+# read from firebase
+def readFromFirebase():
+    recieve_data = firebase.get('user', None)
+    print("Database : ", recieve_data)
+
+
+writeToFirebase("tom", 1212121)
+readFromFirebase()
